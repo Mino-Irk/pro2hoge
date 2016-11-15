ba16d0022e589c348b114dbc7eb8bdec0a06dcdf
Mino-Irk <e155734@gmail.com>
2016-11-15 09:43:52 +0900

    test

---
+++ test.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class test {
+    public static void main(String args[]){
+
+	Scanner scan = new Scanner(System.in);
+	String memo = scan.next();
+
+	System.out.println(memo);
+    }
+}
